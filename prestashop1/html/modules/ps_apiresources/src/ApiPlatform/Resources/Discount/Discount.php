83878efdaa74501110280d630fad55d074900078
speedgrafik <speedgrafik@MacBook-Pro-de-speedgrafik.local>
2026-01-21 14:27:21 +0100

    Description des changements

---
+++ prestashop1/html/modules/ps_apiresources/src/ApiPlatform/Resources/Discount/Discount.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License version 3.0
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
+ */
+
+namespace PrestaShop\Module\APIResources\ApiPlatform\Resources\Discount;
+
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use PrestaShop\Decimal\DecimalNumber;
+use PrestaShop\PrestaShop\Core\Domain\Discount\Command\AddDiscountCommand;
+use PrestaShop\PrestaShop\Core\Domain\Discount\Command\DeleteDiscountCommand;
+use PrestaShop\PrestaShop\Core\Domain\Discount\Command\UpdateDiscountCommand;
+use PrestaShop\PrestaShop\Core\Domain\Discount\Exception\DiscountConstraintException;
+use PrestaShop\PrestaShop\Core\Domain\Discount\Exception\DiscountNotFoundException;
+use PrestaShop\PrestaShop\Core\Domain\Discount\Query\GetDiscountForEditing;
+use PrestaShopBundle\ApiPlatform\Metadata\CQRSCreate;
+use PrestaShopBundle\ApiPlatform\Metadata\CQRSDelete;
+use PrestaShopBundle\ApiPlatform\Metadata\CQRSGet;
+use PrestaShopBundle\ApiPlatform\Metadata\CQRSPartialUpdate;
+use PrestaShopBundle\ApiPlatform\Metadata\LocalizedValue;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ApiResource(
+    operations: [
+        new CQRSGet(
+            uriTemplate: '/discounts/{discountId}',
+            requirements: ['discountId' => '\d+'],
+            CQRSQuery: GetDiscountForEditing::class,
+            scopes: ['discount_read'],
+            CQRSQueryMapping: self::QUERY_MAPPING,
+        ),
+        new CQRSCreate(
+            uriTemplate: '/discounts',
+            validationContext: ['groups' => ['Default', 'Create']],
+            CQRSCommand: AddDiscountCommand::class,
+            CQRSQuery: GetDiscountForEditing::class,
+            scopes: ['discount_write'],
+            CQRSQueryMapping: self::QUERY_MAPPING,
+            CQRSCommandMapping: self::COMMAND_MAPPING,
+        ),
+        new CQRSPartialUpdate(
+            uriTemplate: '/discounts/{discountId}',
+            requirements: ['discountId' => '\d+'],
+            CQRSCommand: UpdateDiscountCommand::class,
+            CQRSQuery: GetDiscountForEditing::class,
+            scopes: ['discount_write'],
+            CQRSQueryMapping: self::QUERY_MAPPING,
+            CQRSCommandMapping: self::COMMAND_MAPPING,
+        ),
+        new CQRSDelete(
+            uriTemplate: '/discounts/{discountId}',
+            CQRSCommand: DeleteDiscountCommand::class,
+            scopes: [
+                'discount_write',
+            ],
+        ),
+    ],
+    exceptionToStatus: [
+        DiscountNotFoundException::class => Response::HTTP_NOT_FOUND,
+        DiscountConstraintException::class => Response::HTTP_UNPROCESSABLE_ENTITY,
+    ],
+)]
+class Discount
+{
+    #[ApiProperty(identifier: true)]
+    public int $discountId;
+    #[Assert\NotBlank(groups: ['Create'])]
+    #[LocalizedValue]
+    public array $names;
+    public int $priority;
+    public bool $enabled;
+    public \DateTimeImmutable $validFrom;
+    public \DateTimeImmutable $validTo;
+    public int $totalQuantity;
+    public int $quantityPerUser;
+    public string $description;
+    public string $code;
+    public int $customerId;
+    public bool $highlightInCart;
+    public bool $allowPartialUse;
+    #[Assert\NotBlank(groups: ['Create'])]
+    public string $type;
+    public ?DecimalNumber $percentDiscount;
+    public ?DecimalNumber $amountDiscount;
+    public int $currencyId;
+    public bool $taxIncluded;
+    public int $productId;
+    public array $combinations;
+    public int $reductionProduct;
+
+    protected const QUERY_MAPPING = [
+        '[localizedNames]' => '[names]',
+        '[active]' => '[enabled]',
+        '[isTaxIncluded]' => '[taxIncluded]',
+    ];
+    protected const COMMAND_MAPPING = [
+        '[names]' => '[localizedNames]',
+        '[enabled]' => '[active]',
+    ];
+}
